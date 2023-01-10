086bf8e129a56f86dfcedb1cfb3c89e83e52bc2b
Deivissondb <deivissondb@hotmail.com>
2023-01-10 11:55:20 -0300

    1.0

---
+++ alura_flutter_curso_7-master/test/bank_model_test.dart
@@ -0,0 +1,24 @@
+import 'package:estilizacao_componentes/models/bank.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('bank model deposit tests', () {
+    test('bank model deposit should turn points into 10', () {
+      final bank = BankModel();
+      bank.deposit(10);
+      expect(bank.points, 10);
+    });
+
+    test('bank model deposit should turn available into 10', () {
+      final bank = BankModel();
+      bank.deposit(10);
+      expect(bank.available, 10);
+    });
+  });
+
+  test('bank model transfer should turn points into 10', () {
+    final bank = BankModel();
+    bank.transfer(10);
+    expect(bank.points, 10);
+  });
+}
